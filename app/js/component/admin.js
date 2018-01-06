c2cde68e49ca9b45ce1c18ecf5356244fa048dcb
Ian Gallacher <igallacher@outlook.com>
2018-01-05 17:30:28 -0800

    Season manager in admin control panel

--- app/js/component/admin.js
+++ app/js/component/admin.js
@@ -7,56 +7,41 @@ import { API_URL } from './app.js'
 export class AdminControlPanel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { file: null };
     if(localStorage.getItem("access_token") === null)
        localStorage.setItem("access_token", "");
-
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.fileUpload = this.fileUpload.bind(this);
   }
 
-  onChange(event) {
+  onChange = (event) => {
     this.setState({
       file: event.target.files[0]
     });
   }
 
-  onSubmit(event) {
-    event.preventDefault();
-    this.fileUpload(this.state.file);
-  }
-
-  addSeasonBtn() {
+  addSeasonBtn = () => {
     console.log("NEW SEASON");
-  }
 
-  fileUpload(file) {
-    // Configure upload.
-    const url = API_URL + "/bots/upload";//"/user-uploads/botdll/upload";
-    const access_token = localStorage.getItem("access_token");
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('bot_name', "asdf");
-    formData.append('user_id', localStorage.getItem("user_id"));
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-        'Authorization': access_token
-      }
-    }
-    console.log(formData);
+      // Configure upload.
+      const url = API_URL + "/seasons";
+      const access_token = localStorage.getItem("access_token");
 
-    // Submit the upload
-    axios.post(url, formData, config)
-    .then(function (response) {
-      console.log("upload response");
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log("ERROR");
-      console.log(error);
-    });
+      let instance = axios.create({
+        headers: {'Authorization': access_token}
+      });
+
+      instance.post(url, {
+        "name": "Season",
+        "summary": [
+          {}
+        ]
+      })
+      .then(function (response) {
+        console.log("upload response");
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log("ERROR");
+        console.log(error);
+      });
   }
 
   render() {
