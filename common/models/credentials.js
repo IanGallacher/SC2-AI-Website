2c423b88a0713701bbe1827736270fe3a3920ea2
Ian Gallacher <igallacher@outlook.com>
2018-03-01 13:21:27 -0800

    Webadmin is a special user, does not have a profile

--- common/models/credentials.js
+++ common/models/credentials.js
@@ -2,6 +2,10 @@
 
 module.exports = function(Credentials) {
   Credentials.observe('after save', function (ctx, next) {
+    // The first user is automatically created, called webadmin.
+    // He does not have a coresponding author to go with him. 
+    if (ctx.instance.username == "webadmin")
+      next();
     ctx.instance.authors.create({
       "username": ctx.instance.username,
       "avatar": "",
