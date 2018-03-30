cb56edea03a9710d96c345f974bd5aeea641e6bd
IanGallacher <igallacher@outlook.com>
2018-03-30 15:42:51 -0700

    Navigation links stay highlighted

--- client-src/js/component/header.js
+++ client-src/js/component/header.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import LoginLogic from './../logic/login.js'
 
 export class Header extends React.Component {
   render() {
     if(this.props.username == "") {
-      var signupButton = <li><Link to="/sign-up">Sign Up</Link></li>;
-      var loginButton = <li><Link to="/login">Login</Link></li>;
+      var signupButton = <li><NavLink to="/sign-up">Sign Up</NavLink></li>;
+      var loginButton = <li><NavLink to="/login">Login</NavLink></li>;
     } else {
-      var profileButton = <li><Link to="/my-profile">Profile</Link></li>;
+      var profileButton = <li><NavLink to="/my-profile">Profile</NavLink></li>;
       var logoutButton = <li onClick={this.props.logout}>
           <a>Log Out</a>
       </li>;
     }
 
     if(this.props.role == "admin") {
-        var adminButton = <li><Link to="/admin-control-panel">Admin</Link></li>;
+        var adminButton = <li><NavLink to="/admin-control-panel">Admin</NavLink></li>;
     }
 
     return (
@@ -25,8 +25,8 @@ export class Header extends React.Component {
           <div className="navbar-header">Starcraft 2 AI Ladder</div>
             <ul className="nav navbar-nav">
               { adminButton }
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/bots">Bots</Link></li>
+              <li><NavLink to="/home">Home</NavLink></li>
+              <li><NavLink to="/bots">Bots</NavLink></li>
               { signupButton }
               { loginButton }
               { profileButton }
