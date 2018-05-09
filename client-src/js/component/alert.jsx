713917d11dc4fb4bd3b77da4b7911b66fd693c9b
IanGallacher <igallacher@outlook.com>
2018-05-09 11:18:30 -0700

    update alert fade out animations

--- client-src/js/component/alert.jsx
+++ client-src/js/component/alert.jsx
@@ -3,38 +3,82 @@ import PropTypes from "prop-types";
 
 import AlertLogic from "./../logic/alert.js";
 
-const Alert = props => {
-  return (
-    <div className={props.cssClass}
-      onClick={() => AlertLogic.removeAlertWithId(props.id)}>
-      <div className="alert-text">
-        {props.message}
-      </div>
-    </div>
-  );
-};
-Alert.propTypes = {
-  cssClass: PropTypes.string,
-  message: PropTypes.string,
-  id: PropTypes.number
-};
+class Alert extends React.Component {
+  constructor() {
+    super();
+    this.state = {visable: false};
+  }
 
-export default class AlertZone extends React.Component {
-  static propTypes = { messages: PropTypes.array };
+  static propTypes = {
+    invisable: PropTypes.bool
+  }
+
+  componentDidMount() {
+    // Creates the element with 0 opacity.
+    // Once visable is true, we transition to 1 opacity.
+    setTimeout(() => this.setState({visable: true}), 0);
+  }
 
   render() {
+    let props = this.props;
+    let visable = this.state.visable ? " alert-visable" : "";
+    if(this.props.invisable) visable = "";
     return (
-      <div className="alert-zone">
-        {
-          (this.props.messages.length > 0) &&
-            this.props.messages.map(message =>
-              <Alert key={message.id}
-                id={message.id}
-                cssClass={message.cssClass}
-                message={message.message}/>
-            )
-        }
+      <div className={props.cssClass + visable}
+        onClick={() => AlertLogic.removeAlertWithId(props.id) }>
+        <div className="alert-text">
+          {props.message}
+        </div>
       </div>
     );
+  }
+}
+
+export default class AlertZone extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { messages: props.messages, destroying: [] };
+  }
+
+  static propTypes = {
+    invisable: PropTypes.bool,
+    messages: PropTypes.array
+  }
+
+  componentWillReceiveProps(props) {
+    // If the prop has removed the element, update the state
+    for(let m of this.state.messages) {
+      if(!props.messages.find(e => e.id === m.id))
+        this._deleteMessage(m);
+    }
+    for(let m of props.messages) {
+      if(!this.state.messages.find(e => e.id === m.id)) {
+        let new_m = this.state.messages;
+        new_m.push(m);
+        this.setState( {messages:  new_m} );
+      }
+    }
+  }
+
+  _deleteMessage(mess) {
+    this.setState( {destroying: this.state.destroying.concat([mess.id])} );
+    setTimeout(() => this.setState({
+      messages: this.state.messages.filter(e => e != mess),
+      destroying: this.state.destroying.filter(ele => ele === mess.id)
+    }), 200);
+  }
+
+  render() {
+    return <div className="alert-zone">
+      {
+        this.state.messages.slice(0).reverse().map((message) =>
+          <Alert key={message.id}
+            id={message.id}
+            invisable={(this.state.destroying.indexOf(message.id) > -1)}
+            cssClass={message.cssClass}
+            message={message.message}/>
+        )
+      }
+    </div>;
   }
 }
