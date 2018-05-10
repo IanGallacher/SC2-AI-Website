b9f45b9207f6e75ef92a1a58a4cd4d50d3270ca4
IanGallacher <igallacher@outlook.com>
2018-05-10 10:57:10 -0700

    chart win percent highlighting

---
+++ client-src/js/component/chart.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  PieChart,
+  Pie,
+  Cell, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Sector
+} from "recharts";
+const RADIAN = Math.PI / 180;
+
+export const SimpleLineChart = (data) => {
+  return (
+    <LineChart width={600} height={300} data={data}
+      margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+      <XAxis dataKey="name"/>
+      <YAxis/>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <Tooltip/>
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  );
+};
+
+const renderActiveShape = (p) => {
+  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+    fill, payload, percent, value } = p;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
+  return (
+    <g>
+      <text
+        x={cx}
+        y={cy}
+        dy={8}
+        textAnchor="middle"
+        fill={fill}>{payload.name}</text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"/>
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey} textAnchor={textAnchor}
+        fill="#333">{`Games: ${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999">{`(Rate ${(percent * 100).toFixed(2)}%)`}</text>
+    </g>
+  );
+};
+
+export class VictoryChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+  }
+
+  static propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      value: PropTypes.number
+    }))
+  }
+
+  onPieEnter = (data, index) => {
+    this.setState({ activeIndex: index });
+  }
+
+  render() {
+    const COLORS = ["#00CC22", "#BB1100", "#FFBB28", "#FF8042"];
+    return <PieChart width={300} height={300}>
+      <Pie
+        activeIndex={this.state.activeIndex}
+        activeShape={renderActiveShape}
+        data={this.props.data}
+        dataKey="value"
+        isAnimationActive={false}
+        cx={150}
+        cy={150}
+        innerRadius={60}
+        outerRadius={80}
+        onMouseEnter={this.onPieEnter}>
+        {
+          this.props.data.map((entry, index) => <Cell
+            key={index} fill={COLORS[index % COLORS.length]}/>)
+        }
+      </Pie>
+    </PieChart>;
+  }
+}
--- client-src/js/routing/bot-profile.js
+++ client-src/js/routing/bot-profile.js
@@ -1,34 +1,13 @@
 import axios from "axios";
 import React from "react";
-import { Link } from "react-router-dom";
 import ReactRouterPropTypes from "react-router-prop-types";
+import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
-import {
-  PieChart,
-  Pie,
-  Cell, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip
-} from "recharts";
 
 import { API_URL } from "./../routing/app.js";
 import LoadingAnimation from "./../component/loading.jsx";
+import { SimpleLineChart, VictoryChart } from "./../component/chart.jsx";
 import ResultTable from "./../component/table.jsx";
-
-
-
-const SimpleLineChart = (data) => {
-  return (
-    <LineChart width={600} height={300} data={data}
-      margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-      <XAxis dataKey="name"/>
-      <YAxis/>
-      <CartesianGrid strokeDasharray="3 3"/>
-      <Tooltip/>
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-  );
-};
-
 
 class BotProfile extends React.Component {
   constructor(props) {
@@ -41,6 +20,16 @@ class BotProfile extends React.Component {
     location: ReactRouterPropTypes.location.isRequired
   }
 
+  componentDidMount() {
+    let axios_url = `${API_URL}/bots/${this.getBotId()}`;
+    axios.get(axios_url)
+      .then(response => this.setState({ bot: response.data }));
+    this.getBotData(this.getBotId());
+  }
+
+  componentWillReceiveProps() {
+    this.getBotData(this.getBotId());
+  }
 
   getBotData(bot_id) {
     axios.get(`${API_URL}/bots/${bot_id}`)
@@ -55,79 +44,6 @@ class BotProfile extends React.Component {
       bot_id = params.get("bot_id");
     }
     return bot_id;
-  }
-
-  componentDidMount() {
-    let axios_url = `${API_URL}/bots/${this.getBotId()}`;
-    axios.get(axios_url)
-      .then(response => this.setState({ bot: response.data }));
-    this.getBotData(this.getBotId());
-  }
-
-  componentWillReceiveProps() {
-    this.getBotData(this.getBotId());
-  }
-
-  renderLabel = (args) => {
-    let { cx, cy, name, midAngle, innerRadius, outerRadius, percent } = args;
-    const radius = innerRadius + (outerRadius - innerRadius) + 20;
-    const RADIAN = 3.14/180;
-    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-      <text x={x} y={y} fill="black" textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central">
-        {`${name}: ${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
-  }
-
-  render() {
-    let bot = this.state.bot;
-    if (bot === null) return <div className="trading-card-horizontal">
-      <LoadingAnimation/>
-    </div>;
-
-    const data = [
-      {name: "Victories", value: bot.win_count},
-      {name: "Defeats", value: bot.match_count - bot.win_count}
-    ];
-    const lineData = [
-      {name: "Page A", uv: 4000, pv: 2400, amt: 2400},
-      {name: "Page B", uv: 3000, pv: 1398, amt: 2210},
-      {name: "Page C", uv: 2000, pv: 9800, amt: 2290},
-      {name: "Page D", uv: 2780, pv: 3908, amt: 2000},
-      {name: "Page E", uv: 1890, pv: 4800, amt: 2181},
-      {name: "Page F", uv: 2390, pv: 3800, amt: 2500},
-      {name: "Page G", uv: 3490, pv: 4300, amt: 2100},
-    ];
-    const COLORS = ["#00CC22", "#BB1100", "#FFBB28", "#FF8042"];
-    return (
-      <React.Fragment>
-        <div className="trading-card-horizontal">
-          <title>{`Bot Name: ${bot.name}`}</title>
-          <Link to={`/authors/?author_id=${bot.owner_id}`}>
-            {`Bot Author: ${bot.author}`}
-          </Link>
-          <br/>
-          <PieChart width={300} height={300}>
-            <Pie
-              data={data}
-              dataKey="value"
-              cx={150}
-              cy={150}
-              outerRadius={80}
-              isAnimationActive={false}>
-              {
-                data.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>)
-              }
-            </Pie>
-          </PieChart>
-          {SimpleLineChart(lineData)}
-        </div>
-      </React.Fragment>
-    );
   }
 
   renderBotTable(bot_table) {
@@ -171,6 +87,40 @@ class BotProfile extends React.Component {
         ]
       }
     />;
+  }
+
+  render() {
+    let bot = this.state.bot;
+    if (bot === null) return <div className="trading-card-horizontal">
+      <LoadingAnimation/>
+    </div>;
+
+    const data = [
+      {name: "Victories", value: bot.win_count},
+      {name: "Defeats", value: bot.match_count - bot.win_count}
+    ];
+    const lineData = [
+      {name: "Page A", uv: 4000, pv: 2400, amt: 2400},
+      {name: "Page B", uv: 3000, pv: 1398, amt: 2210},
+      {name: "Page C", uv: 2000, pv: 9800, amt: 2290},
+      {name: "Page D", uv: 2780, pv: 3908, amt: 2000},
+      {name: "Page E", uv: 1890, pv: 4800, amt: 2181},
+      {name: "Page F", uv: 2390, pv: 3800, amt: 2500},
+      {name: "Page G", uv: 3490, pv: 4300, amt: 2100},
+    ];
+    return (
+      <React.Fragment>
+        <div className="trading-card-horizontal">
+          <title>{`Bot Name: ${bot.name}`}</title>
+          <Link to={`/authors/?author_id=${bot.owner_id}`}>
+            {`Bot Author: ${bot.author}`}
+          </Link>
+          <br/>
+          <VictoryChart data={data}/>
+          {SimpleLineChart(lineData)}
+        </div>
+      </React.Fragment>
+    );
   }
 }
 export default withRouter(BotProfile);
