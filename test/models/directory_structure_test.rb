5066526d02613ab148b103a2f1a95f14629f8813
IanGallacher <igallacher@outlook.com>
2018-06-10 12:48:39 -0700

    Add more directory structure tests

--- test/models/directory_structure_test.rb
+++ test/models/directory_structure_test.rb
@@ -5,7 +5,13 @@ class DirectoryStructureTest < ActiveSupport::TestCase
   test "has replay folder" do
      assert File.directory? "public/replay"
   end
-  test "has upload folder" do
+  test "has user-upload folder" do
      assert File.directory? "public/user-upload"
+  end
+  test "has dll folder" do
+     assert File.directory? "public/user-upload/dll"
+  end
+  test "has avatar folder" do
+     assert File.directory? "public/user-upload/avatar"
   end
 end
