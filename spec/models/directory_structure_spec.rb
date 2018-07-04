929ecb232042eeb708302224ac5d452a6336d529
Ian Gallacher <igallacher@outlook.com>
2018-07-03 20:39:41 -0700

    replace test system with rspec

---
+++ spec/models/directory_structure_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'Directory Structure' do
+  it 'should have the correct directories' do
+    expect(File.directory? 'public/replay').to be true
+    expect(File.directory? 'public/user-upload').to be true
+    expect(File.directory? 'public/user-upload/dll').to be true
+    expect(File.directory? 'public/user-upload/avatar').to be true
+  end
+end
