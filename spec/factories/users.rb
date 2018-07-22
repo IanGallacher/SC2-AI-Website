ac2fe0e94e38d037ddf9580b1641e7e4defed56b
IanGallacher <igallacher@outlook.com>
2018-07-22 13:52:20 -0700

    add larger variety of usernames to avoid duplicates when running tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::StarTrek.character }
+    username { Faker::Name.name }
     email { Faker::Internet.email }
     after(:build) { |u| u.password_confirmation = u.password = 'asdfasdf' }
   end
