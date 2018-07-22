5cae0f8ca7e077a147c2cc9032aab5c5d350ad36
IanGallacher <igallacher@outlook.com>
2018-07-22 13:54:09 -0700

    enforce unique usernames when running tests to prevent duplicates

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { Faker::Name.name }
+    username { Faker::StarTrek.unique.character }
     email { Faker::Internet.email }
     after(:build) { |u| u.password_confirmation = u.password = 'asdfasdf' }
   end
