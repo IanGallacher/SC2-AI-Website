43d1baa697ffad02d545fe00f8fe6ea26ec0a194
Ian Gallacher <igallacher@outlook.com>
2018-08-18 19:37:12 -0700

    fix test

--- spec/models/game_result_model_spec.rb
+++ spec/models/game_result_model_spec.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'rails_helper'
 
 describe 'Game Result Model -' do
@@ -15,13 +14,3 @@ describe 'Game Result Model -' do
 
   it 'should update mmr'
 end
-=======
-require 'rails_helper'
-
-describe 'Game Result Model -' do
-  it 'should save the replay to disk'
-  it 'should save the replay to disk'
-  it 'should update mmr'
-  it 'should setup relationships'
-end
->>>>>>> master
