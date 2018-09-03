44807148cfa143e25b4dceeb6731cc7952674ebe
Ian Gallacher <igallacher@outlook.com>
2018-09-03 11:39:58 -0700

    add folder requried to run application

--- spec/models/directory_structure_spec.rb
+++ spec/models/directory_structure_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'Directory Structure -' do
   it 'should have the correct directories' do
+    expect(File.directory? 'public/cache/season_zip').to be true
     expect(File.directory? 'public/replay').to be true
     expect(File.directory? 'public/user-upload').to be true
     expect(File.directory? 'public/user-upload/dll').to be true
     expect(File.directory? 'public/user-upload/avatar').to be true
-  end
 end
