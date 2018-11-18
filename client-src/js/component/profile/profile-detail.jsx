cfd5b32f37ed08b59aacf5d705c72780b3e2c0a4
Ian Gallacher <igallacher@outlook.com>
2018-11-17 16:28:31 -0800

    show patreon even when github is nil

--- client-src/js/component/profile/profile-detail.jsx
+++ client-src/js/component/profile/profile-detail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function ProfileDetail(icon, label, description, is_link) {
-  if(description == null) return;
+  if(description == null || description == "") return;
   let content = <div><i className={`fab ${icon}`}/> {`${label}: ${description}`}</div>;
   if(is_link == true) return <a href={description}>
     {content}
