e926c86cf4dfbbae38c16b31b1e169d620bec989
Ian Gallacher <igallacher@outlook.com>
2019-01-19 16:06:08 -0800

    cleanup sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,7 @@ class SessionsController < ApplicationController
 
     if resource.valid_password?(params[:password])
       sign_in(:user, resource)
-      render json: {
-          success: true
-      }
+      render json: { success: true }
       return
     end
     invalid_login(true)
@@ -23,26 +21,27 @@ class SessionsController < ApplicationController
       render json: { success: true }
     else
       render json: {
-          success: false,
-          message: "No session."
+        success: false,
+        message: "No session."
       }
     end
   end
 
   protected
-    def ensure_params_exist
-      return unless params.has_key?(:login) && params.has_key?(:password)
-      render json: {
-          success: false,
-          message: "Invalid request parameters."
-      }, status: :unprocessable_entity
-    end
 
-    def invalid_login(password_invalid = false)
-      warden.custom_failure!
-      render json: {
-          success: false,
-          message: password_invalid ? "Incorrect password." : "Matching account not found."
-      }, status: :unauthorized
-    end
+  def ensure_params_exist
+    return unless params.has_key?(:login) && params.has_key?(:password)
+    render json: {
+      success: false,
+      message: "Invalid request parameters."
+    }, status: :unprocessable_entity
+  end
+
+  def invalid_login(password_invalid = false)
+    warden.custom_failure!
+    render json: {
+      success: false,
+      message: password_invalid ? "Incorrect password." : "Matching account not found."
+    }, status: :unauthorized
+  end
 end
