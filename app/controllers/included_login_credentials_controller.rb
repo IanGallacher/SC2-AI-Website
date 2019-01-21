9707543678a350bf6dce5a6573345d16ae62b486
Ian Gallacher <igallacher@outlook.com>
2019-01-20 16:22:27 -0800

    do not nest username and password in authentication

--- app/controllers/included_login_credentials_controller.rb
+++ app/controllers/included_login_credentials_controller.rb
@@ -2,9 +2,10 @@ class IncludedLoginCredentialsController < ApplicationController
   before_action :authenticate_user
 
   def authenticate_user
-    authentication = params[:authentication]
-    return unless authentication.present?
-    user = User.find_for_database_authentication(username: authentication[:username])
-    sign_in(:user, user) if user.valid_password?(authentication[:password])
+    username = params[:username]
+    password = params[:password]
+    return unless username.present? && password.present?
+    user = User.find_for_database_authentication(username: username)
+    sign_in(:user, user) if user.valid_password?(password)
   end
 end
